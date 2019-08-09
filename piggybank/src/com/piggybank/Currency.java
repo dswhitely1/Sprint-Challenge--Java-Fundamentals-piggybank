ae6920e62d2398792ec5a6d9807dc9675c464922
Donald Whitely <dswhitely1@gmail.com>
2019-08-09 11:10:37 -0400

    Abstract Class Defined

---
+++ piggybank/src/com/piggybank/Currency.java
@@ -0,0 +1,25 @@
+package com.piggybank;
+
+public abstract class Currency {
+  protected int coin;
+
+  public Currency() {
+    this.coin = 1;
+  }
+
+  public Currency(int coin) {
+    this.coin = coin;
+  }
+
+  public int getCoin() {
+    return this.coin;
+  }
+
+  public void setCoin(int coin) {
+    this.coin = coin;
+  }
+
+  public abstract double getValue();
+
+  public abstract String toString(String s);
+}
